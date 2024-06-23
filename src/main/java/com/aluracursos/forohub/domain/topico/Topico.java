2d80c66e35eb444ba5b15818ae88812df5c8f2a3
JuanC-Cabrera <juan.cabrera1409@alumnos.udg.mx>
2024-06-23 09:16:13 -0600

    feat(entity): create Topico entity for JPA mapping

---
+++ src/main/java/com/aluracursos/forohub/domain/topico/Topico.java
@@ -0,0 +1,29 @@
+package com.aluracursos.forohub.domain.topico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Table(name = "topicos")
+@Entity(name = "Topico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private Date fechaCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+
+}
