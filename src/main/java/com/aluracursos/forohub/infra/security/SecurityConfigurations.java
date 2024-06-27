5d9a9a7dfb03ba9023282fc7367fed148e70d988
JuanC-Cabrera <juan.cabrera1409@alumnos.udg.mx>
2024-06-27 13:45:11 -0600

    feat: integrate JWT token generation and verification in TokenService
    - Implemented token generation with user details and expiration.
    - Added token verification method to validate and extract subject.
    - Configured secret from application.properties for token signing.

--- src/main/java/com/aluracursos/forohub/infra/security/SecurityConfigurations.java
+++ src/main/java/com/aluracursos/forohub/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package com.aluracursos.forohub.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,16 +13,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    public SecurityFilter securityFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf->csrf.disable())
+                .sessionManagement((sess-> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)))
+                .authorizeHttpRequests((request -> request
+                        .requestMatchers(HttpMethod.POST,"/registrar").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                        .anyRequest().authenticated()))
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 
     @Bean
--- src/main/java/com/aluracursos/forohub/infra/security/TokenService.java
+++ src/main/java/com/aluracursos/forohub/infra/security/TokenService.java
@@ -2,8 +2,10 @@ package com.aluracursos.forohub.infra.security;
 
 import com.aluracursos.forohub.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +31,27 @@ public class TokenService {
         } catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+
+    public String getSubject(String token) {
+        if (token == null){
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+            verifier = JWT.require(algorithm)
+                    .withIssuer("forohub")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTCreationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null){
+            throw new RuntimeException("Verifier invalido");
+        }
+        return verifier.getSubject();
     }
 
     private Instant generarFechaExpiracion(){
