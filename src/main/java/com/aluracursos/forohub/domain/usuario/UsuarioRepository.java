25233d1da64eef5d67e275c889392072f1de1ca8
JuanC-Cabrera <juan.cabrera1409@alumnos.udg.mx>
2024-06-26 19:05:24 -0600

    feat: Configure API for stateless security using Spring Security

--- src/main/java/com/aluracursos/forohub/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/aluracursos/forohub/domain/usuario/UsuarioRepository.java
@@ -1,10 +1,13 @@
 package com.aluracursos.forohub.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByNombre(String nombre);
 
     boolean existsByEmail(String email);
+
+    UserDetails findByNombre(String username);
 }
