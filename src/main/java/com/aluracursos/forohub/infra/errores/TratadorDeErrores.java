b8f8a57407e37ceb3b4c1c1b1ee45b59d005d81c
JuanC-Cabrera <juan.cabrera1409@alumnos.udg.mx>
2024-06-26 18:16:10 -0600

    refactor(errors): restructure error handling classes

---
+++ src/main/java/com/aluracursos/forohub/infra/errores/TratadorDeErrores.java
@@ -0,0 +1,52 @@
+package com.aluracursos.forohub.infra.errores;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> tratarError404(EntityNotFoundException e) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> tratarError400(MethodArgumentNotValidException e) {
+        List<DatosErrorValidacion> errores = e.getFieldErrors().stream()
+                .map(DatosErrorValidacion::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(ValidacionDeIntegridad.class)
+    public ResponseEntity<?> errorHandlerValidacionesIntegridad(ValidacionDeIntegridad e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> errorHandlerValidacionesDeNegocio(ValidationException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleGeneralExceptions(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor.");
+    }
+
+    private record DatosErrorValidacion(String campo, String error) {
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+
+}
